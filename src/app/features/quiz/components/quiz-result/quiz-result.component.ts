ad23a7a877268f76d7f088eb94d5703635a9731c
Saba Mchedlishvili <sabamchedlishvili17@gmail.com>
2022-12-05 17:02:21 +0400

    fix reload function

--- src/app/features/quiz/components/quiz-result/quiz-result.component.ts
+++ src/app/features/quiz/components/quiz-result/quiz-result.component.ts
@@ -1,21 +1,26 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-quiz-result',
   templateUrl: './quiz-result.component.html',
-  styleUrls: ['./quiz-result.component.scss']
+  styleUrls: ['./quiz-result.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class QuizResultComponent implements OnInit {
 
   @Input() correctCount!: number;
   @Input() questionCount!: number;
 
-  constructor() { }
+  constructor(private router: Router, public location: Location) { }
 
   ngOnInit(): void {
   }
 
   reLoad(): void {
-    window.location.reload();
+    this.router.navigateByUrl("/", { skipLocationChange: true }).then(() => {
+      this.router.navigate(["quiz"]);
+      });
   }
 }
