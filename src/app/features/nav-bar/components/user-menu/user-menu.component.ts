d28bbc7bc990d849794462777c706bf51e56dd58
Saba Mchedlishvili <sabamchedlishvili17@gmail.com>
2022-12-12 22:24:00 +0400

    fix nav bar(user menu) issue & add snackBar to Log out

--- src/app/features/nav-bar/components/user-menu/user-menu.component.ts
+++ src/app/features/nav-bar/components/user-menu/user-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthorizationService } from 'src/app/core/services/authorization.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-menu',
@@ -10,13 +11,16 @@ export class UserMenuComponent implements OnInit{
   ngOnInit(): void {
   }
 
-  @Output() toggleMunu = new EventEmitter<boolean>();
+  @Output() toggleMenu = new EventEmitter<boolean>();
 
-  constructor(public auth: AuthorizationService) {}
+  constructor(public auth: AuthorizationService, private _snackBar: MatSnackBar) {}
 
   logOut(): void {
     this.auth.isLoggedIn = false;
-    this.toggleMunu.emit(false);
+    this.toggleMenu.emit(false);
   }
 
+  openSnackBar(msg: string) {
+    this._snackBar.open(msg,'', {duration: 3000, horizontalPosition: 'start'});
+  }
 }
